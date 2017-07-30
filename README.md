6a266f9e9efaea3c6d7435f373261e40e9f8e17a
zhaoxiaolin <zhaoxiaolin@iyunxiao.com>
2017-07-30 18:22:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## 核心方法
+
+1、setTimeout() - 用于在指定的毫秒数后调用函数或者计算表达式；
+  只执行一次code，若果要多次调用，请使用setInterval或者code自身再调用一次setTimeout.
+2、clearTimeout() - 清除setTimeout返回的id值，从而取消setTimeout设置的延时；
+3、setInterval() － 按照指定周期调用函数或计算表达式，多次调用。
+4、clearInterval() － 清除setInterval返回的id值，从而取消setInterval设置的延时；
+5、Math.floor  - 对一个数进行向下取整；
+6、Math.random － 返回0～1之间的一个随机数；
+
+## 设计
+  - 1、点击开始游戏；
+  - 2、倒计时开始，分数等置0；
+  - 3、老鼠不断显示、隐藏；
+  - 4、倒计时归0结束，或者点击结束按钮结束；
+
+## 功能
+  - 1、得分统计；
+  - 2、成功率；
+  - 3、老鼠出现、隐藏；
+  - 4、判断是否点中；
+  - 5、结果显示；
---
+++ main.js
@@ -0,0 +1,84 @@
+var td = new Array(),   // 初始化一个数组，保存每个格子地鼠
+    playing = false,    // 游戏是否开始
+    score = 0,          // 分数
+    beat = 0,           // 鼠标点击次数
+    success = 0,        // 命中率
+    knock = 0,          // 打中老鼠的次数
+    countdown = 30,     // 倒计时
+    interId = null,
+    timeId = null,
+    audioElem = null;
+
+  // 游戏开始
+  function getStart() {
+    playing = true;
+    interId = setInterval("showMouse()", 1000);
+    audioElem = document.getElementById('playaudio');
+    document.formhead.score.value = score;
+    document.formhead.success.value = success;
+    showCountDown();
+  }
+  // 倒计时
+  function showCountDown() {
+    document.formhead.countdown.value = countdown;
+    if(countdown === 0) {
+      gameOver();
+      return;
+    }
+    countdown += -1;
+    timeId = setTimeout("showCountDown()", 1000);
+  }
+  // 随机出现老鼠
+  function showMouse() {
+    if(playing) {
+      var current = Math.floor(Math.random()*25);
+      document.getElementById("td["+ current +"]").innerHTML = '<img src="./img/mouse.png">';
+      setTimeout("document.getElementById('td[" + current + "]').innerHTML = ''", 3000);
+    }
+  }
+  // 清空老鼠
+  function clearMouse() {
+    for(var i = 0; i <= 24; i++) {
+      document.getElementById("td["+ i + "]").innerHTML = "";
+    }
+  }
+  // 点击事件
+  function hit(id) {
+    if(playing === false) {
+      alert('请点击开始游戏');
+      return;
+    }
+    beat += 1;
+    if(document.getElementById("td["+ id +"]").innerHTML !== "") {
+      score += 1;
+      knock += 1;
+      success = (knock/beat).toFixed(2);
+      audioElem.play();
+      setTimeout("audioElem.pause()", 1000);
+      document.formhead.score.value = score;
+      document.formhead.success.value = success;
+      document.getElementById("td["+ id +"]").innerHTML = "";
+      return;
+    }
+    success = (knock/beat).toFixed(2);
+    document.formhead.score.value = score;
+    document.formhead.success.value = success;
+  }
+  // 停止所有计时
+  function timeStop() {
+    clearInterval(interId);
+    clearTimeout(timeId);
+  }
+  // 游戏结束
+  function gameOver() {
+    timeStop();
+    playing = false;
+    audioElem.pause();
+    clearMouse();
+    alert("游戏结束！\n 你获得的分数为："+ score +"\n 命中率为："+success);
+    success = 0;
+    score = 0;
+    knock = 0;
+    beat = 0;
+    countdown = 30;
+  }
